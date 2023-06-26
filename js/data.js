ebb7abba999b8b46d2ad1e9e01befc672c6041e9
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-06-26 15:59:17 +0300

    implemented dashboard

--- js/data.js
+++ js/data.js
@@ -1,5 +1,43 @@
 ﻿const boards = [
   {
+    "title": "General",
+    "accentColor": "#c09030",
+    "items": [
+      {
+        "title": "Project Environments - E-commerce - Confluence",
+        "link": "https://gosyqor.atlassian.net/wiki/spaces/EC/pages/848625787/Project+Environments"
+      },
+      {
+        "title": "[EC-1315] Refinement - Jira",
+        "link": "https://gosyqor.atlassian.net/browse/EC-1315"
+      },
+      {
+        "title": "[EC-1693] Alexander onboarding - Jira",
+        "link": "https://gosyqor.atlassian.net/browse/EC-1693"
+      },
+      {
+        "title": "HURMA",
+        "link": "https://yellowmedia-vjet.hurma.work/"
+      },
+      {
+        "title": "Installation - Tailwind CSS",
+        "link": "https://tailwindcss.com/docs/installation"
+      },
+      {
+        "title": "Team Reports - Worklog author - Jira",
+        "link": "https://gosyqor.atlassian.net/plugins/servlet/ac/com.gebsun.plugins.jira.startwork/worklog-user-worklogs-report-page#!/?datem=useFromTo&from=2023-06-01&groupBy=com.gebsun.fields.worklogAuthor&jusers=712020%3A6328bb7c-b2e3-442a-9d33-40b4c08511e2&period=days&timeZone=user&to=2023-06-30&wlFormat=hours"
+      },
+      {
+        "title": "All Diamonds - Magnified in 360° HD | JamesAllen.com",
+        "link": "https://www.jamesallen.com/loose-diamonds/all-diamonds/"
+      },
+      {
+        "title": "Daily Stand Up",
+        "link": "https://meet.google.com/njq-isew-dew?authuser=1"
+      },
+    ]
+  },
+  {
     "title": "Diamond Club",
     "accentColor": "#c09030",
     "items": [
@@ -36,5 +74,75 @@
         "link": "https://devapi.omara.sk/public/apidoc/"
       }
     ]
-  }
+  },
+  {
+    "title": "A-Link",
+    "accentColor": "#c09030",
+    "items": [
+      {
+        "title": "AL board - Agile Board - Jira",
+        "link": "https://gosyqor.atlassian.net/jira/software/c/projects/AL/boards/46?assignee=712020%3A6328bb7c-b2e3-442a-9d33-40b4c08511e2"
+      },
+      {
+        "title": "vjet_developers / alink_vue_fe — Bitbucket",
+        "link": "https://bitbucket.org/vjet_developers/alink_vue_fe/src/master/"
+      },
+      {
+        "title": "ALINK Tradeshows + Website 2023 – Figma",
+        "link": "https://www.figma.com/file/jpgul0dmaBZcGM5laIJlgR/ALINK-Tradeshows-%2B-Website-2023?type=design&node-id=970-806&t=sc827Jx8BuZOimiZ-0"
+      },
+      {
+        "title": "Image Assets 05.12.23 – Google Диск",
+        "link": "https://drive.google.com/drive/u/1/folders/1pJTSMXHw0v90fUKMdw__0XcPCfS1g1DD"
+      },
+      {
+        "title": "DEV - A-link",
+        "link": "https://alinkjewelry.devsyqor.com/"
+      },
+    ]
+  },
+  {
+    "title": "AdminFE",
+    "accentColor": "#c09030",
+    "items": [
+      {
+        "title": "vjet_developers / e-comm-adminfe — Bitbucket",
+        "link": "https://bitbucket.org/vjet_developers/e-comm-adminfe/src/master/"
+      },
+      {
+        "title": "apiDoc: E-Commerce - 1.0.0",
+        "link": "https://devapi.diamondclub.com/public/apidoc/#api-AdminIntegrations-PutAdminApiV1IntegrationsId"
+      },
+    ]
+  },
+  {
+    "title": "Penny Preville",
+    "accentColor": "#c09030",
+    "items": [
+      {
+        "title": "Penny Preville Kanban - Agile Board - Jira",
+        "link": "https://gosyqor.atlassian.net/jira/software/c/projects/PPV/boards/36?quickFilter=64&assignee=712020%3A6328bb7c-b2e3-442a-9d33-40b4c08511e2"
+      },
+      {
+        "title": "vjet_developers / pennypervile_vue — Bitbucket",
+        "link": "https://bitbucket.org/vjet_developers/pennypervile_vue/commits/"
+      },
+      {
+        "title": "Figma",
+        "link": "https://www.figma.com/file/e3ErcAZkq4Q8doEUuAoNCo/PP-Website---Handoff-2023?type=design&node-id=117-544&t=i6rfcgizwA9idSi7-0"
+      },
+      {
+        "title": "DEV - pennypreville.devsyqor.com",
+        "link": "https://pennypreville.devsyqor.com/"
+      },
+      {
+        "title": "Adminka - DEV",
+        "link": "https://pp-admin.devsyqor.com/products"
+      },
+      {
+        "title": "Adminka - STAGE",
+        "link": "https://pp-admin-stage.devsyqor.com/products"
+      },
+    ]
+  },
 ]
--- js/index.js
+++ js/index.js
@@ -1,3 +1,53 @@
 ﻿const { createApp } = Vue;
 console.log(boards, createApp)
-// const app = 
+
+const app = createApp({
+  el: '#app',
+  template: `
+    <boards :items="boards" />
+  `,
+  data: () => ({
+    boards: boards
+  })
+});
+
+app.component('boards', {
+  template: `
+    <div class="boards">
+      <board v-for="board in items" :boardData="board" />
+    </div>
+  `,
+  props: {
+    items: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+});
+
+app.component('board', {
+  template: `
+    <div class="board">
+      <h1 class="board-title">{{ boardData.title }}</h1>
+      <ul>
+        <li v-for="item in boardData.items" @click="open(item.link)">
+          <div class="title">{{ item.title }}</div>
+          <div class="link">{{ item.link }}</div>
+        </li>
+      </ul>
+    </div>
+  `,
+  props: {
+    boardData: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  methods: {
+    open(link) {
+      window.open(link, '_blank');
+    }
+  }
+});
+
+app.mount('#app');
